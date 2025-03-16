1147b4574ecfcdb72e36b9bbbc7f0a92d6bed118
gianpaolodatu <gianpaolodatu1@gmail.com>
2025-03-17 00:24:48 +0100

    changes for step 1.5

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
 const Part = (props) => {
   return(
     <p>
-      {props.part} {props.exercise}
+      {props.name} {props.exercise}
     </p>
   )
 }
@@ -15,9 +15,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
-      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
-      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+      <Part name={props.parts[0].name} exercise={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercise={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercise={props.parts[2].exercises}/>
     </>
   )
 
@@ -30,22 +30,32 @@ const Footer = (props) => {
 
 
 const App = () =>{
-  const course = 'Half Stack application development!!!'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14 
 
-  const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const totalExerciseNumber = course.parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
     <>
-      <Header courseName = {course} />
-      <Content parts = {parts} exercises ={exercises} />
-      <Footer number = {exercises1 + exercises2 + exercises3} />
+      <Header courseName = {course.name} />
+      <Content parts = {course.parts} />
+      <Footer number = {totalExerciseNumber} />
     </>
   )
 }
